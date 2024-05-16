44c60e7a4b2ee3a86003e4d218d933b736debe49
katsiev1997 <katsiev1997@gmail.com>
2024-05-17 01:11:52 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
 import { useEffect, useState } from "react";
 import { Themes } from "./theme";
+import axios from "axios";
+
+type Phrase = {
+  id: number;
+  rus: string;
+  ing: string;
+  trscp: string;
+};
 
 function App() {
-  const [theme, setTheme] = useState("");
+  const [theme, setTheme] = useState("ПРИВЕТСТВИЕ");
+  const [loading, setLoading] = useState(false);
+  const [phrases, setPhrases] = useState<Phrase[]>();
+  const themes = Themes;
   useEffect(() => {
+    const getData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get(
+          `https://e62d26507a8d1382.mokky.dev/words?subject=${theme}`
+        );
+        setPhrases(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     if (theme !== "") {
-      alert(theme);
+      getData();
     }
   }, [theme]);
-  const themes = Themes;
   return (
-    <div>
-      {themes.map((item) => (
-        <div
-          onClick={() => setTheme(item)}
-          className="pl-5 font-semibold cursor-pointer hover:bg-zinc-200 max-h-14 min-h-8 h-min  w-full max-w-xs flex items-center"
-        >
-          {item}
+    <div className="flex">
+      <div className="hidden lg:w-1/4 lg:block">
+        {themes.map((item, i) => (
+          <div
+            key={i}
+            onClick={() => setTheme(item)}
+            className="pl-5 font-semibold cursor-pointer hover:bg-zinc-200 max-h-14 min-h-8 h-min  w-full max-w-xs flex items-center"
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="pl-3 w-screen overscroll-x-none">
+          {phrases?.map((item) => (
+            <div
+              key={item.id}
+              className={
+                item.id % 2 === 0
+                  ? "flex items-center gap-1 text-sm min-h-10 bg-slate-100"
+                  : "flex items-center gap-1 text-sm min-h-10"
+              }
+            >
+              <div className="w-1/3 lg:w-1/4">{item.rus}</div>
+              <div className="w-1/3 lg:w-1/4">{item.ing}</div>
+              <div className="w-1/3 lg:w-1/4">{item.trscp}</div>
+            </div>
+          ))}
         </div>
-      ))}
+      )}
     </div>
   );
 }
