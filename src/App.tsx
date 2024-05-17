23d98ce4e95882905b8fd244e68e6d3f0dfd91fb
katsiev1997 <katsiev1997@gmail.com>
2024-05-17 20:34:15 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import axios from "axios";
 type Phrase = {
   id: number;
   rus: string;
-  ing: string;
+  ing: string; 
   trscp: string;
 };
 
 function App() {
   const [theme, setTheme] = useState("ПРИВЕТСТВИЕ");
+  const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
   const [phrases, setPhrases] = useState<Phrase[]>();
   const themes = Themes;
@@ -32,39 +33,61 @@ function App() {
     }
   }, [theme]);
   return (
-    <div className="flex">
-      <div className="hidden lg:w-1/4 lg:block">
-        {themes.map((item, i) => (
-          <div
-            key={i}
-            onClick={() => setTheme(item)}
-            className="pl-5 font-semibold cursor-pointer hover:bg-zinc-200 max-h-14 min-h-8 h-min  w-full max-w-xs flex items-center"
-          >
-            {item}
+    <>
+      <header>
+        <button onClick={() => setOpen(!open)}>open</button>
+        {open && (
+          <div className="">
+            {themes.map((item, i) => (
+              <div
+                key={i}
+                onClick={() => {
+                  setTheme(item);
+                  setOpen(false);
+                }}
+                className="pl-5 font-semibold cursor-pointer hover:bg-zinc-200 max-h-14 min-h-8 h-min  w-full max-w-xs flex items-center"
+              >
+                {item}
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div className="pl-3 w-screen overscroll-x-none">
-          {phrases?.map((item) => (
+        )}
+      </header>
+
+      <div className="flex">
+        <div className="hidden lg:w-1/4 lg:block">
+          {themes.map((item, i) => (
             <div
-              key={item.id}
-              className={
-                item.id % 2 === 0
-                  ? "flex items-center gap-1 text-sm min-h-10 bg-slate-100"
-                  : "flex items-center gap-1 text-sm min-h-10"
-              }
+              key={i}
+              onClick={() => setTheme(item)}
+              className="pl-5 font-semibold cursor-pointer hover:bg-zinc-200 max-h-14 min-h-8 h-min  w-full max-w-xs flex items-center"
             >
-              <div className="w-1/3 lg:w-1/4">{item.rus}</div>
-              <div className="w-1/3 lg:w-1/4">{item.ing}</div>
-              <div className="w-1/3 lg:w-1/4">{item.trscp}</div>
+              {item}
             </div>
           ))}
         </div>
-      )}
-    </div>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div className="px-3 w-screen overscroll-x-none">
+            {phrases?.map((item) => (
+              <div
+                key={item.id}
+                className={
+                  item.id % 2 === 0
+                    ? "flex items-center gap-1 text-sm min-h-10 bg-slate-100"
+                    : "flex items-center gap-1 text-sm min-h-10"
+                }
+              >
+                <div className="w-1/3 lg:w-1/4">{item.rus}</div>
+                <div className="w-1/3 lg:w-1/4">{item.ing}</div>
+                <div className="w-1/3 lg:w-1/4">{item.trscp}</div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
